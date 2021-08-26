4ddb017aa3c5e8a6b1f377217c10602d80ac3b5b
NeuroSim <ysm1030@gmail.com>
2021-08-26 17:41:16 -0400

    Update Tile.cpp

--- Training_pytorch/NeuroSIM/Tile.cpp
+++ Training_pytorch/NeuroSIM/Tile.cpp
@@ -846,6 +846,7 @@ void TileCalculatePerformance(const vector<vector<double> > &newMemory, const ve
 		}
 		*leakage = PEleakage*numPE + accumulationNM->leakage + inputBufferNM->leakage + outputBufferNM->leakage;
 	}
+	return 0;
 }
 
 
